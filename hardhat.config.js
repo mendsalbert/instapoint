d6b99238372124350bc88401c804c8b45a707111
mendsalbert <mendsalbert@gmail.com>
2025-02-22 22:17:04 +0000

    feat: Add Sepolia testnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const privateKey = process.env.PRIVATE_KEY_SONIC_BLAZE_TESTNET;
+
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 4202,
+    },
+
+    sonic_blaze_testnet: {
+      url: "https://rpc.blaze.soniclabs.com",
+      accounts: [privateKey],
+    },
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  allowUnlimitedContractSize: true,
+  throwOnTransactionFailures: true,
+  throwOnCallFailures: true,
+  loggingEnabled: true,
 };
